0c5bd5bfede6169341b8c6082f0278301c14b2b3
Dave Jones <49728965+dhjonesclc@users.noreply.github.com>
2022-02-16 11:47:23 -0800

    Update env and parser options for node 16 (#7)
    * update parser and env to es2021
    * update dev dependencies and minimum required eslint version

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 'use strict';
 
 module.exports = {
-  extends: ['prettier'],
-  parserOptions: {
-    ecmaVersion: 9
-  },
-  env: {
-    es6: true,
-    browser: true, // browser global variables.
-    node: true, // Node.js global variables and Node.js-specific rules.
-    commonjs: true, // defines require() and define() as global variables as per the amd spec.
-    mocha: true, // adds all of the Mocha testing global variables.
-    jquery: true // jquery global variables.
-  }
+    extends: ['prettier'],
+    parserOptions: {
+        ecmaVersion: 2021
+    },
+    env: {
+        es2021: true,
+        browser: true, // browser global variables.
+        node: true, // Node.js global variables and Node.js-specific rules.
+        commonjs: true, // defines require() and define() as global variables as per the amd spec.
+        mocha: true, // adds all of the Mocha testing global variables.
+        jquery: true // jquery global variables.
+    }
 };
